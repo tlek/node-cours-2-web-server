5d30015018fadd6de1f50bb36ee5ac0a8c70b1e2
tlek <sompornc@hotmail.com>
2019-02-18 14:51:46 -0600

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -22,9 +23,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req,res,next)=>{  //for maintaining purpose comment it when go live
-    res.render('maintenance.hbs'); 
-});
+// app.use((req,res,next)=>{  //for maintaining purpose comment it when go live
+//     res.render('maintenance.hbs'); 
+// });
 
 app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear',() => {
@@ -38,7 +39,7 @@ hbs.registerHelper('screamIt', (text) => {
 app.get('/',(req,res) => {
     res.render('home.hbs',{
         pageTitle: 'This is my home',
-        welcome: 'Welcome to my world'
+        welcome: 'Welcome to my world..'
 
     });
 });
@@ -53,7 +54,7 @@ app.get('/about',(req,res) => {
 
 app.get('/help',(req,res)=>{
     res.send('Help Page');
-})
+})  
 
 app.get('/bad', (req,res) =>{
     res.send({
@@ -62,6 +63,6 @@ app.get('/bad', (req,res) =>{
 });
 
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000');
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
 });
